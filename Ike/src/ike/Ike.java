4a8a60e7610e128653eb868df885110adc93afa2
toscanox <toscanox@toscanox-MOBL.amr.corp.intel.com>
2017-02-23 17:51:43 -0600

    Switch to chrome

--- Ike/src/ike/Ike.java
+++ Ike/src/ike/Ike.java
@@ -6,6 +6,7 @@
 package ike;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 /**
@@ -18,11 +19,13 @@ public class Ike {
      */
     public static void setProps(){
         System.setProperty("webdriver.gecko.driver", "C:\\geckodriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
     }
     
     public static void main(String[] args) throws InterruptedException {
         setProps();
-        WebDriver driver = new FirefoxDriver();
+        //WebDriver driver = new FirefoxDriver();
+        WebDriver driver = new ChromeDriver();
         driver.get("https://sise2mx.ikeasistencia.com/");
         driver.findElement(By.id("Usr")).clear();
         driver.findElement(By.id("Usr")).sendKeys("cmantenimiento");
@@ -30,8 +33,9 @@ public class Ike {
         driver.findElement(By.id("Pass")).sendKeys("022017jR");
         driver.findElement(By.id("btnLogin")).click();
         for (int i=0; i<1; i=0){
-            Thread.sleep(3000);
+            Thread.sleep(3100);
             driver.findElement(By.xpath("//div[2]/div[2]/table/tbody/tr/td")).click();
+            //#MensajeTimeOut > table:nth-child(2) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(4) > td:nth-child(2) > button:nth-child(1)
         }
     }
 }
